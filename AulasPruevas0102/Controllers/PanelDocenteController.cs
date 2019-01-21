46cc591517c45b53777bd9588d79f0c62fcb2285
Nadelina <gs.nathy@yahoo.es>
2019-01-21 10:43:25 -0600

    Panel docentes

--- AulasPruevas0102/Controllers/PanelDocenteController.cs
+++ AulasPruevas0102/Controllers/PanelDocenteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AulasPruevas0102.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace AulasPruevas0102.Controllers
 {
     public class PanelDocenteController : Controller
     {
+        private asignacionEntities db = new asignacionEntities();
         // GET: PanelDocente
         public ActionResult Index()
         {
             return View();
         }
-    }
+        [ChildActionOnly]
+        public ActionResult Horarios()
+        {
+            var query = from a in db.HorariosFijos select a;
+            return PartialView("_HorariosFijos", query.ToList());
+        }
+        }
 }
